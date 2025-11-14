a9d00f0ecb98902716f48307014860bee9856ae2
andre-maia51 <acmc.0410@gmail.com>
2025-11-13 22:16:36 -0300

    hotfix: fix the baseUrl of authApi

--- lib/api/authApi.ts
+++ lib/api/authApi.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const authApi = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_AUTH_API_URL,
-});
+export const authApi = axios.create({
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/auth`,
+})
 
 authApi.interceptors.request.use(
   (config) => {
